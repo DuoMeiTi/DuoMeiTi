5836ec502bd1c93b82d162244196fe3bebf4b793
kad0108 <kad0108@foxmail.com>
2015-09-15 14:28:08 +0800

    repertory_delete()
    repertory_delete()

--- DuoMeiTi/src/admin/RepertoryAction.java
+++ DuoMeiTi/src/admin/RepertoryAction.java
@@ -1,13 +1,14 @@
 package admin;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 import com.opensymphony.xwork2.ActionSupport;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import model.Repertory;
 
@@ -16,6 +17,7 @@ public class RepertoryAction extends ActionSupport{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private int rtId;
 	private String rtType;
 	private String rtNumber;
 	private List<Repertory> repertory_list;
@@ -25,6 +27,13 @@ public class RepertoryAction extends ActionSupport{
 	/*status 1: OK
 			0:  save fail*/
 
+	public int getRtId() {
+		return rtId;
+	}
+
+	public void setRtId(int rtId) {
+		this.rtId = rtId;
+	}
 	public String getRtType() {
 		return rtType;
 	}
@@ -78,7 +87,6 @@ public class RepertoryAction extends ActionSupport{
 	
 	public String insert(){
 		
-		
 		if(rtNumber.equals("")||rtNumber.equals("")){
 			 this.status = "0";
 		}
@@ -95,22 +103,43 @@ public class RepertoryAction extends ActionSupport{
 				session.save(rt);
 				session.getTransaction().commit();
 				session.close();
+				
 				this.status = "1";
+				this.rtId = rt.getRtId();
 				this.add_repertory_html = util.Util.fileToString("/jsp/admin/widgets/add_repertory.html");
 			}
 			catch(Exception e)
 			{
 				e.printStackTrace();
 			}
-			
-			
-			
 			//JOptionPane.showMessageDialog(null, "提交成功");
 		}
+		return SUCCESS;
+	}
+	
+	public String delete(){
 		
-//		System.out.println(rtNumber);
-//		System.out.println(rtType);
-		
+		Repertory rt = new Repertory();
+		//List<Object> paramList = new ArrayList<Object>();
+		Session session = model.Util.sessionFactory.openSession();
+		//System.out.println(rtId);
+		Criteria q = session.createCriteria(Repertory.class).add(Restrictions.eq("rtId", rtId));//eq("字段名","变量名")Integer.parseInt
+		List paramList = q.list();
+//		System.out.println("123456");
+		//System.out.println(paramList);
+		if(paramList.isEmpty()){
+			this.status = "0";//error;
+		}else{
+			session.beginTransaction();
+			session.delete(paramList.get(0));
+			session.getTransaction().commit();
+			
+			this.status = "1";//ok
+		}
+		session.close();
+		//paramList.add(rt.getRtId());
+		//session.delete(paramList);
+
 		return SUCCESS;
 	}
 	
