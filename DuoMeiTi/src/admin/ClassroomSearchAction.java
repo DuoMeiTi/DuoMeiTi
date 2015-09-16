98f2dae949b81a2717244f35a1604a158108c2b3
liuzhuocpp <liuzhuocpp@qq.com>
2015-09-16 11:02:27 +0800

    syn

--- DuoMeiTi/src/admin/ClassroomSearchAction.java
+++ DuoMeiTi/src/admin/ClassroomSearchAction.java
@@ -2,38 +2,30 @@ package admin;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import model.Classroom;
 
+import org.apache.struts2.ServletActionContext;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ClassroomSearchAction {
+public class ClassroomSearchAction extends ActionSupport{
 	//private String classroom_num;  //教室号
 	//private String principle;  //负责人
 	private String search;
 	private List<Classroom> classroom_list;//教室信息
 	private String searchselect;
 	private String status;
+	//private HttpServletRequest request;
+	private String classroominfo_html;
 	/*
 	 * status 0: OK
 	 * 		  1: 查询关键字为空
 	 */
-	
-	/*public String getClassroom_num() {
-		return classroom_num;
-	}
-	public void setClassroom_num(String classroom_num) {
-		this.classroom_num = classroom_num;
-	}
-	public String getPrinciple() {
-		return principle;
-	}
-	public void setPrinciple(String principle) {
-		this.principle = principle;
-	}*/
 	
 	public String getSearch() {
 		return search;
@@ -60,10 +52,17 @@ public class ClassroomSearchAction {
 	public void setClassroom_list(List<Classroom> classroom_list) {
 		this.classroom_list = classroom_list;
 	}
-	
+	public String getClassroominfo_html() {
+		return classroominfo_html;
+	}
+	public void setClassroominfo_html(String classroominfo_html) {
+		this.classroominfo_html = classroominfo_html;
+	}
 	public String classroom_search() throws Exception
 	{
-		
+		//request=ServletActionContext.getRequest();
+//		Classroom clr = new Classroom();
+//		System.out.println("action");
 		if(search==null)
 		{
 			this.status = "error: search key is null";
@@ -85,12 +84,26 @@ public class ClassroomSearchAction {
 				q.add(Restrictions.eq("principal", search));
 			}
 			//System.out.println(searchselect + " " + search);
-			List ul = q.list();
-			session.beginTransaction();
-			//session.save(cr);
-			session.getTransaction().commit();
-			this.status = "0";
+			classroom_list= q.list();
+			System.out.println(classroom_list.size());
+//			Classroom clr = new Classroom();
+			for(int i=0;i<classroom_list.size();i++){
+//				clr.setClassroom_num(classroom_list.get(i).classroom_num);
+//				clr.setRepertorys(classroom_list.get(i).repertorys);
+//				clr.setCapacity(classroom_list.get(i).capacity);
+//				clr.setPrincipal(classroom_list.get(i).principal);
+//				session.beginTransaction();
+//				session.save(clr);
+//				session.getTransaction().commit();
+				this.classroominfo_html = util.Util.fileToString("/jsp/homepage/widgets/classroominfo.html");
+//				session.close();
+			}
 			session.close();
+			
+			this.status = "0";
+			//this.classroom_id = clr.getId();
+			
+			//request.getSession().setAttribute("classroom_list",classroom_list);
 		}
 		return ActionSupport.SUCCESS;
 		
---
+++ DuoMeiTi/src/model/RepairRecord.java
@@ -0,0 +1,27 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+public class RepairRecord {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public int id;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
+	@JoinColumn(name="device")
+	public Repertory device;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
+	@JoinColumn(name="classroom")
+	public Classroom classroom;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
+	@JoinColumn(name="repairman")
+	public StudentProfile repairman;
+	
+	@Column(length=100)
+	public String repairdetail;
+	
+	public java.sql.Date repairdate;
+}
