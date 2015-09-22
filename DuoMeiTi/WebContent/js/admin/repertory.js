0ccbbdc127520a4fae1938bf651a9b101c9e9252
liuzhuocpp <liuzhuocpp@qq.com>
2015-09-22 16:51:08 +0800

    OK

--- DuoMeiTi/WebContent/js/admin/repertory.js
+++ DuoMeiTi/WebContent/js/admin/repertory.js
@@ -24,22 +24,53 @@ $(document).find("#rtDevice").change(function() {
 })
 
 $(document).on("click", "#rtSave", function() {
-	if ($("#rtDevice").val() == "" || $("#rtType").val() == "") {
+	if ( $("#rtDevice").val() == "" || $("[name=rtType]").val() == "" ) {
 		alert("输入不能为空！");
 		return;
 	}
 	var params = $("#repertory_form").serialize();// 序列化表单值→ Json；
-	alert(typeof  params);
-	alert(params);
-	alert(decodeURIComponent(params,true));
+//	alert(typeof  params);
+//	alert(params);
+//	alert(decodeURIComponent(params,true));
+	var fd = new FormData();
+	var cnt = $("#repertory_form");
+ 	alert($(cnt).find("#rtDevice").val());
+ 	alert($(cnt).find("[name=rtType]").val());
+
+// 	fd.append("test", "YESYESYES");
+ 	fd.append("rtDevice", $(cnt).find("#rtDevice").val());
+ 	fd.append("rtType",$(cnt).find("[name=rtType]").val());
+ 	fd.append("rtNumber", $(cnt).find("[name=rtNumber]").val());
+ 	fd.append("rtVersion", $(cnt).find("[name=rtVersion]").val());
+ 	fd.append("rtFactorynum", $(cnt).find("[name=rtFactorynum]").val());
+ 	
+ 	
+// 	fd.append("rtDevice", $(cnt).find("#rtDevice").val());
+// 	fd.append("rtDevice", $(cnt).find("#rtDevice").val());
+ 	
+
 	// ajax方法通过HTTP请求加载远程数据；
-	$.ajax({
-		url : '/admin/repertory_insert',
-		type : 'post',
-		dataType : 'json',
-		data : params,
-		success : repertoryCallback
-	});
+    $.ajax({  
+        url:'/admin/repertory_insert' ,  
+        type: "POST",  
+        data: fd,  
+        async: true,  
+        cache: false,  
+        contentType: false,  
+        processData: false,  
+         success: repertoryCallback,
+//         error: function (returndata) {  
+//             alert(returndata);  
+//         }  
+   });  
+
+//	$.ajax({
+//		url : '/admin/repertory_insert',
+//		type : 'post',
+//		dataType : 'json',
+//		data : fd,
+//		success : repertoryCallback
+//	});
 
 })
 
