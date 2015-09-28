91393aeb321289d7603af0c6f1ec6744bdf84a59
liuzhuocpp <liuzhuocpp@qq.com>
2015-09-28 14:54:46 +0800

    add jdbc

--- DuoMeiTi/src/homepage/HomeAction.java
+++ DuoMeiTi/src/homepage/HomeAction.java
@@ -30,10 +30,10 @@ public class HomeAction
 //	 Criteria q =
 //session.createCriteria(User.class).add(Restrictions.eq("username",
 //username));
-//		Session session = model.Util.sessionFactory.openSession();
+		Session session = model.Util.sessionFactory.openSession();
 		
-//		check_list = session.createCriteria(model.CheckRecord.class).list();
-//		session.close();
+		check_list = session.createCriteria(model.CheckRecord.class).list();
+		session.close();
 //		model.CheckRecord cr = (model.CheckRecord)check_list.get(0);
 		
 		
