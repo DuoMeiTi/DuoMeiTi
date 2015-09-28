1f5ddc0262660c593e4b8523350f2897b2f365a3
liuzhuocpp <liuzhuocpp@qq.com>
2015-09-28 18:19:42 +0800

    modify fileupload

--- DuoMeiTi/src/util/FileUploadBaseAction.java
+++ DuoMeiTi/src/util/FileUploadBaseAction.java
@@ -23,4 +23,23 @@ public class FileUploadBaseAction extends ActionSupport{
     public File file; //上传的文件
     public String fileFileName; //文件名称
     public String fileContentType; //文件类型
+	public File getFile() {
+		return file;
+	}
+	public void setFile(File file) {
+		this.file = file;
+	}
+	public String getFileFileName() {
+		return fileFileName;
+	}
+	public void setFileFileName(String fileFileName) {
+		this.fileFileName = fileFileName;
+	}
+	public String getFileContentType() {
+		return fileContentType;
+	}
+	public void setFileContentType(String fileContentType) {
+		this.fileContentType = fileContentType;
+	}
+    
 }
