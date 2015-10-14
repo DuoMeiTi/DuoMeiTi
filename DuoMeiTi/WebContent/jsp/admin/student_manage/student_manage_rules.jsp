27136306d0b0451018d30ff7c22f34ac09f8ddea
wutongshu1993 <wutongshu1993@qq.com>
2015-10-14 17:34:41 +0800

    规章制度

--- DuoMeiTi/WebContent/jsp/admin/student_manage/student_manage_rules.jsp
+++ DuoMeiTi/WebContent/jsp/admin/student_manage/student_manage_rules.jsp
@@ -14,10 +14,10 @@
 	<br>
 	<center>
 	<form  class="form-horizontal" method="post">
-	<textarea rows="20" cols="150" id="text" name="text"><s:property value="textShow"/></textarea>
+	<textarea rows="20" cols="150" id="ruleText" name="ruleText"><s:property value="textShow"/></textarea>
 	<br>
 	
-	<button class="btn  btn-primary" onclick="rules_edit()" >确定</button>
+	<button id="ruleEditButton"class="btn  btn-primary"  type="button">确定</button>
 	</form>
 	
 	<label for="name_id">还没做：</label>
@@ -32,29 +32,35 @@
 		
 		
 	</div>
-	
-	
-</layout:override>
 
-<script >
-function rules_edit(){
-	var text = $("#text").val();
-	alert(text); 
+
+	
+	<script >
+
+$(document).on("click", "#ruleEditButton", function(){
+	var ruleText = $("#ruleText").val();
+	alert(ruleText); 
 	$.ajax({
-		url : '/admin/student_manage/rule_edit',
+		url : '/admin/student_manage/ruleEdit',
 		type : 'post',
 		dataType : 'json',
-		data : {//发送到服务器的数据
-			"text" : text
+		data : 
+		{
+			"ruleText" : ruleText,
 		},
 		success : addRulesCallback
 	});
 	 
-}
-
+});
 function addRulesCallback(data){
 	alert("ok");
 	
 }
 </script>
+	
+	
+	
+	
+</layout:override>
+
 <%@ include file="base.jsp" %>
--- DuoMeiTi/src/student/StudentAction.java
+++ DuoMeiTi/src/student/StudentAction.java
@@ -2,11 +2,16 @@ package student;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import model.Rules;
 import model.TeachBuilding;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+
 import utility.DatabaseOperation;
 
 import com.opensymphony.xwork2.ActionContext;
@@ -14,6 +19,7 @@ import com.opensymphony.xwork2.ActionContext;
 public class StudentAction extends ActionSupport{
 	
 	private List<BuildingsInfo> teahBuildings;
+	private String textShow;
 	
 	public class BuildingsInfo{
 		public String buildingName;
@@ -32,6 +38,14 @@ public class StudentAction extends ActionSupport{
 		this.teahBuildings = teahBuildings;
 	}
 
+	public String getTextShow() {
+		return textShow;
+	}
+
+	public void setTextShow(String textShow) {
+		this.textShow = textShow;
+	}
+
 	public String chooseClass() throws Exception{
 		
 		List fields = new ArrayList<String>();
@@ -48,6 +62,23 @@ public class StudentAction extends ActionSupport{
 			teahBuildings.add(new BuildingsInfo(name,id));
 		}
 		return ActionSupport.SUCCESS;
+	}
+	public String ruleShow() throws Exception{
+		Session session = model.Util.sessionFactory.openSession();
+		Criteria q = session.createCriteria(Rules.class);
+		Rules temp;
+		if (q.list().size() > 0) {
+			temp = (Rules)q.list().get(0);//
+			textShow = temp.getText();
+			System.out.println(textShow);
+			textShow = textShow.replace("\n", "<br/>&nbsp;&nbsp;&nbsp;&nbsp;");
+			System.out.println("***********"+textShow);
+		}
+		else {
+			textShow =" ";
+		}
+		session.close();
+		return SUCCESS;
 	}
 	
 }
