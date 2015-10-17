765eb16fb7e3b5533df9cf9a9c792b009a091fa7
liuzhuocpp <liuzhuocpp@qq.com>
2015-10-17 20:25:26 +0800

    更好的分页了

--- DuoMeiTi/src/homepage/HomeAction.java
+++ DuoMeiTi/src/homepage/HomeAction.java
@@ -62,15 +62,17 @@ public class HomeAction extends PageGetBaseAction
 		Session session = model.Util.sessionFactory.openSession();
 		Criteria q = session.createCriteria(model.ResourceFilePath.class);
 		
-		file_path_list = this.makeCurrentPageList(q, 5); // 获取当前页第一页
+		file_path_list = this.makeCurrentPageList(q, 10); // 根据代表总体的Criteria 获取当前页元素的List，这个效率高，应尽量使用这个
+//		file_path_list = this.makeCurrentPageList(q.list(), 10); //根据代表总体的List 获取当前页元素的List
+		
+		
 		session.close();
+		
 		if(this.getIsAjaxTransmission()) // 这是ajax 传输
 		{
-			file_path_html = util.Util.getJspOutput("/jsp/admin/HomepageModify/ResourceFileTable.jsp");
-				
+			file_path_html = util.Util.getJspOutput("/jsp/admin/HomepageModify/ResourceFileTable.jsp");				
 			return "getPage";
 		}
-		
 		return ActionSupport.SUCCESS;
 	}
 
--- DuoMeiTi/src/util/PageGetBaseAction.java
+++ DuoMeiTi/src/util/PageGetBaseAction.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
@@ -23,7 +24,7 @@ public class PageGetBaseAction extends ActionSupport  {
 	
 	//page_size:每一页显示的元素条数
 	//total_num:元素的总数，元素的条数从0开始计数
-	public static int getTotalPageNum(int page_size, int total_num)
+	private static int getTotalPageNum(int page_size, int total_num)
 	{
 		int ans = total_num / page_size;
 		if(total_num % page_size > 0) ans ++;
@@ -34,7 +35,7 @@ public class PageGetBaseAction extends ActionSupport  {
 	//page_size:每一页显示的元素条数
 	//total_num:元素的总数，元素的条数从0开始计数
 	// 返回一个长度为2的数组：第一个元素表示这个页的起始条目的索引， 第二个元素表示这个页应该容纳的元素数目
-	public static int[] getPageRange(int page_num, int page_size, int total_num)
+	private static int[] getPageRange(int page_num, int page_size, int total_num)
 	{
 		int[] res = new int[2];
 		
@@ -58,10 +59,31 @@ public class PageGetBaseAction extends ActionSupport  {
 		return res;
 	}
 	
+	
+	// 根据l 所表示整体List返回页大小为page_size并且页码为this.currentPageNum 的List
+	public List makeCurrentPageList(List l, int page_size)
+	{
+		this.totalPageNum = getTotalPageNum(page_size, l.size());
+		int[] res = getPageRange(currentPageNum, page_size, l.size());
+		if(this.getIsAjaxTransmission())
+		{
+			this.paginationHtml = util.Util.getJspOutput("/jsp/base/widgets/paginationTable.jsp");
+		}
+		else 
+		{
+			this.paginationHtml = null;
+		}
 
-	// newCurrentPageNum  新的页面值！！！
-	// 根据q 所表示整体数据量返回页大小为page_size的List
-	// 同时设置this.currentPageNum
+		
+		List ans = new ArrayList<Object>() ;
+		for(int i = res[0]; i < res[0] + res[1]; ++ i)
+		{
+			ans.add(l.get(i));
+		}
+		return ans;
+	}
+	
+	// 根据数据库的Criteria q 所表示整体数据量返回页大小为page_size的List
 	public List makeCurrentPageList(Criteria q, int page_size)
 	{		
 		q.setProjection(Projections.rowCount());
