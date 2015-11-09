86ab0a40f6601f2b93311faedf5b9815cfb59f96
WuChao <wuchao155425@gmail.com>
2015-11-09 15:52:06 +0800

    添加签到表

---
+++ DuoMeiTi/src/model/CheckInRecord.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+
+public class CheckInRecord {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int id;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public StudentProfile getStudent() {
+		return student;
+	}
+
+	public void setStudent(StudentProfile student) {
+		this.student = student;
+	}
+
+	public Timestamp getRecordtime() {
+		return recordtime;
+	}
+
+	public void setRecordtime(Timestamp recordtime) {
+		this.recordtime = recordtime;
+	}
+
+	@ManyToOne
+	@JoinColumn
+	public StudentProfile student;
+	
+	@Column
+	public Timestamp recordtime;
+
+}
