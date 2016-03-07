25f791dd3f1d4aa0a244fc3be62b14eb30ac0c5b
liuzhuocpp <liuzhuocpp@qq.com>
2016-03-07 14:35:03 +0800

    add   grant sql

--- DuoMeiTi/WebContent/sql/create.sql
+++ DuoMeiTi/WebContent/sql/create.sql
@@ -2,3 +2,5 @@ create database duomeiti  default  character set utf8;
 
 set global       interactive_timeout=31536000;
  show global variables like '%timeout';
+ 
+ grant all privileges on *.*  to root@'%'  identified by  'root';
