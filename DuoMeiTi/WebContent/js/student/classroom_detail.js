41603bb0a81e614b7b7d28f5bc2a7fa24d8de458
liuzhuocpp <liuzhuocpp@qq.com>
2016-04-10 15:15:35 +0800

    simply classroom

--- DuoMeiTi/WebContent/js/student/classroom_detail.js
+++ DuoMeiTi/WebContent/js/student/classroom_detail.js
@@ -10,7 +10,7 @@ $(document).on('click','#move2repair',function(){
 	move_device_id = $(this).parents("[device_id]").attr("device_id");// attr所选元素属性值
 	/*alert(class_Id+"  "+move_device_id);*/
 	var params = {
-			"move_class_id" : class_Id,
+			"classroomId" : class_Id,
 			"move_device_id" : move_device_id,
 			"opt" : 1,
 		};
@@ -33,7 +33,7 @@ $(document).on('click','#move2bad',function(){
 	move_device_id = $(this).parents("[device_id]").attr("device_id");// attr所选元素属性值
 	/*alert(class_Id+"  "+move_device_id);*/
 	var params = {
-			"move_class_id" : class_Id,
+			"classroomId" : class_Id,
 			"move_device_id" : move_device_id,
 			"opt" : 1,
 		};
@@ -61,7 +61,7 @@ $(document).on('click','.move2class',function(){
 	classroom_id = $("#classroomid").text();
 	/*alert(rtID+classroom_id);*/
 	var params = {
-			"classroomid" : classroom_id,
+			"classroomId" : classroom_id,
 			"rtID" : rtID
 	}
 	
@@ -87,7 +87,7 @@ function alter_device() {
 	var classroomid = $("#classroomid").text();
 	/*alert(classroomid);*/
 	var params = {
-			"classroomid" : classroomid,
+			"classroomId" : classroomid,
 		};
 	$.ajax({
         url: '/student/student_classroom_json/alterdevice',
@@ -112,7 +112,8 @@ $(document).on('click','#submit_repair_record',function(){
 	/*alert(selectDeviceId+" "+repairdetail);*/
 	var params = {
 			"repairdetail" : repairdetail,
-			"deviceId" : selectDeviceId
+			"deviceId" : selectDeviceId,
+			"classroomId":$("#classroomid").text(),
 		};
 		
 		$.ajax({
@@ -157,7 +158,7 @@ function checkrecord_submit() {
 	//alert(checkrecord_detail);
 	var params = {
 		"checkdetail" : checkrecord_detail,
-		"classroomid": classroomid
+		"classroomId": classroomid
 	};
 	
 	$.ajax({
@@ -221,104 +222,4 @@ function addcallback(data) {
 
 
 /*以上都是我新写的*/
-/*function checkrecord_submit() {
-	var params = {
-		"checkdetail": checkrecord_detail
-	};
-	$.ajax({
-		url : '/student/student_classroom_json/checkrecord_save',
-		type : 'post',
-		dataType : 'json',
-		data : params,
-		success : checkrecord_save_callback
-	});
-}
-function checkrecord_save_callback(data) {
-	if(data.savestatus == "fail") 
-		alert("fail!");
-	else if(data.savestatus == "success") {
-		var login_username = $("#login_user_name").text();
-		var checkrecord_detail = $("#checkdetail").val();
-		var today = new Date().format("yy-MM-dd");
-		var table = $("#checkrecord_table");
-		var rowcount = $(table).find("tr").length;
-//		alert(rowcount);
-		$(table).find("tr:last").after("<tr><td width=\"20%\"></td><td></td><td></td></tr>");
-		var row = $(table).find("tr:last");
-		$(row).find("td:eq(0)").text(login_username);
-		$(row).find("td:eq(1)").text(checkrecord_detail);
-		$(row).find("td:eq(2)").text(today);
-		if(rowcount >= 6) {
-			$(table).find("tr:eq(1)").remove();
-//			var v = $(table).find("tr:eq(1)").find("td:eq(1)").text();
-//			var last = $(table).find("tr:last").find("td:eq(1)").text();
-//			alert(v+" "+last);
-		}
-//		alert("success!");
-	}
-	$('#check-record-modal').modal('hide');
-}
-
-
-var selectDeviceId;
-var selectIndex;
-
-function openRepairMoadl(index) {
-	selectDeviceId = $("#device-" + index + " .device-id-span:first").text();
-	selectIndex = index;
-	var num = $("#device-" + index + " .device-num-span:first").text();
-	var type = $("#device-" + index + " .device-type-label:first").text();
-//	alert(selectDeviceId+" "+num+" "+type);
-	$('#repair-record-modal').modal('show');
-	$('#repair-record-modal').on('shown.bs.modal', function () {
-		  $("#selectType").text(type);
-		  $("#selectNum").text(num);
-	})
-}
-
-function repairrecord_submit() {
-	var repairdetail = $("#repairdetail").val();
-	var params = {
-			"repairdetail" : repairdetail,
-			"deviceId" : selectDeviceId
-		};
-		
-		$.ajax({
-			url : '/student/student_classroom_json/repairrecord_save',
-			type : 'post',
-			dataType : 'json',
-			data : params,
-			success : repairrecord_save_callback
-		});
-}
-function repairrecord_save_callback(data) {
-	if(data.savestatus == "fail") 
-		alert("fail!");
-	else if(data.savestatus == "success") {
-		var login_username = $("#login_user_name").text();
-		var type = $("#device-" + selectIndex + " .device-type-label:first").text();
-		var repairdetail = $("#repairdetail").val();
-		var today = new Date().format("yy-MM-dd");
-		var table = $("#repairrecord_table");
-		var rowcount = $(table).find("tr").length;
-//		alert(rowcount);
-		$(table).find("tr:last").after("<tr><td width=\"20%\"></td><td></td><td></td><td></td></tr>");
-		var row = $(table).find("tr:last");
-		$(row).find("td:eq(0)").text(login_username);
-		$(row).find("td:eq(1)").text(type);
-		$(row).find("td:eq(2)").text(repairdetail);
-		$(row).find("td:eq(3)").text(today);
-		if(rowcount >= 6) {
-			$(table).find("tr:eq(1)").remove();
-//			var v = $(table).find("tr:eq(1)").find("td:eq(1)").text();
-//			var last = $(table).find("tr:last").find("td:eq(1)").text();
-//			alert(v+" "+last);
-		}
-//		alert("success!");
-	}
-	$('#repair-record-modal').modal('hide');
-}
-
-
-*/
 
--- DuoMeiTi/src/student/ClassroomDetailAction.java
+++ DuoMeiTi/src/student/ClassroomDetailAction.java
@@ -23,80 +23,85 @@ import com.opensymphony.xwork2.ActionSupport;
 
 import db.MyHibernateSessionFactory;
 
-public class ClassroomDetailAction extends ActionSupport{
-	public int build_id;
-	
-	public int classroom_id;
-	
-	public List<Repertory> repertory;
-	
-	public List<CheckRecord> checkrecords;
-	
-	public List<RepairRecord> repairrecords;
-	public List classroom_repertory_list;
-	
-	public List<Repertory> rtClass;
-	
-	public String execute() {
-		Session session = model.Util.sessionFactory.openSession();
-		//query current select classroom
-		String repertory_hql="select r from Repertory r where r.classroom="+classroom_id;
-		repertory=session.createQuery(repertory_hql).list();
-		ActionContext.getContext().getSession().remove("classroom_id");
-		ActionContext.getContext().getSession().put("classroom_id", classroom_id);
-
-		
-		//query at most 5 checkrecord
-		Criteria checkrecord_criteria = session.createCriteria(CheckRecord.class).setFetchMode("classroom", FetchMode.SELECT).setFetchMode("checkman", FetchMode.SELECT);
-		
-		checkrecord_criteria.add(Restrictions.eq("classroom.id", classroom_id));
-		checkrecord_criteria.addOrder(Order.asc("checkdate"));
-		long check_rowCount = (Long) checkrecord_criteria.setProjection(  
-                Projections.rowCount()).uniqueResult();
-		int checkrecord_start = ((int) check_rowCount) > 5 ? ((int) check_rowCount) - 5 : 0;
-		checkrecord_criteria.setProjection(null);
-		checkrecord_criteria.setFirstResult(checkrecord_start);
-		checkrecord_criteria.setMaxResults(5);
-		checkrecords = checkrecord_criteria.list();
-
-		long repair_rowCount = (Long) session.createQuery("select count(*) from RepairRecord as rd left join rd.device as ry left join ry.classroom as cm where cm.id=" + classroom_id).uniqueResult();
-		int repairrecord_start = ((int) repair_rowCount) > 5 ? ((int) repair_rowCount) - 5 : 0;
-//		System.out.println("s:"+repairrecord_start);
-		repairrecords = (List) session.createQuery("select rd "
-												+ "from RepairRecord as rd "
-												+ "left join rd.device as ry "
-												+ "left join ry.classroom as cm  "
-												+ "where cm.id=" + classroom_id + " order by rd.repairdate").setFirstResult(repairrecord_start).setMaxResults(5).list();
-		
-		
-		
-		
-//		List classroom_repertory_list;
-		
-		classroom_repertory_list = session.createCriteria(model.Repertory.class).add(Restrictions.eq("classroom.id", classroom_id)).list();
-		
-//		System.out.println("JJ");
-//		System.out.println(classroom.repertorys);
-//		System.out.println(classroom_repertory_list);
-		
-		session.close();
-		return SUCCESS;
-	}
-
-	public int getBuild_id() {
-		return build_id;
-	}
-
-	public void setBuild_id(int build_id) {
-		this.build_id=build_id;
-	}
-
-	public int getClassroom_id() {
-		return classroom_id;
-	}
-
-	public void setClassroom_id(int classroom_id) {
-		this.classroom_id = classroom_id;
-	}
-
-}
+//public class ClassroomDetailAction extends ActionSupport{
+//	public int build_id;
+//	
+//	public int classroom_id;
+//	
+//	public List<Repertory> repertory;
+//	
+//	public List<CheckRecord> checkrecords;
+//	
+//	public List<RepairRecord> repairrecords;
+//	public List classroom_repertory_list;
+//	
+//	public List<Repertory> rtClass;
+//	
+//	public String execute() {
+//		Session session = model.Util.sessionFactory.openSession();
+//		//query current select classroom
+//		String repertory_hql="select r from Repertory r where r.classroom="+classroom_id;
+//		repertory=session.createQuery(repertory_hql).list();
+//		ActionContext.getContext().getSession().remove("classroom_id");
+//		ActionContext.getContext().getSession().put("classroom_id", classroom_id);
+//
+//		
+//		//query at most 5 checkrecord
+//		checkrecords = session.createCriteria(CheckRecord.class)
+//				.add(Restrictions.eq("classroom.id", classroom_id))
+//				.addOrder(Order.desc("id"))
+//				.setMaxResults(5)
+//				.list();;
+//		
+////		checkrecord_criteria.add(Restrictions.eq("classroom.id", classroom_id));
+////		checkrecord_criteria.addOrder(Order.desc("id"));
+////		long check_rowCount = (Long) checkrecord_criteria.setProjection(  
+////                Projections.rowCount()).uniqueResult();
+////		int checkrecord_start = ((int) check_rowCount) > 5 ? ((int) check_rowCount) - 5 : 0;
+////		checkrecord_criteria.setProjection(null);
+////		checkrecord_criteria.setFirstResult(checkrecord_start);
+////		checkrecord_criteria.setMaxResults(5);
+////		checkrecords = checkrecord_criteria.list();
+//
+//		long repair_rowCount = (Long) session.createQuery("select count(*) from RepairRecord as rd left join rd.device as ry left join ry.classroom as cm where cm.id=" + classroom_id).uniqueResult();
+////		int repairrecord_start = ((int) repair_rowCount) > 5 ? ((int) repair_rowCount) - 5 : 0;
+////		System.out.println("s:"+repairrecord_start);
+//		repairrecords = (List) session.createQuery("select rd "
+//												+ "from RepairRecord as rd "
+//												+ "left join rd.device as ry "
+//												+ "left join ry.classroom as cm  "
+//												+ "where cm.id=" + classroom_id + " desc by rd.repairdate")
+//										.setMaxResults(5).list();
+//		
+//		
+//		
+//		
+////		List classroom_repertory_list;
+//		
+//		classroom_repertory_list = session.createCriteria(model.Repertory.class).add(Restrictions.eq("classroom.id", classroom_id)).list();
+//		
+////		System.out.println("JJ");
+////		System.out.println(classroom.repertorys);
+////		System.out.println(classroom_repertory_list);
+//		
+//		session.close();
+//		return SUCCESS;
+//	}
+//
+//	public int getBuild_id() {
+//		return build_id;
+//	}
+//
+//	public void setBuild_id(int build_id) {
+//		this.build_id=build_id;
+//	}
+//
+//	public int getClassroom_id() {
+//		return classroom_id;
+//	}
+//
+//	public void setClassroom_id(int classroom_id) {
+//		this.classroom_id = classroom_id;
+//	}
+//
+//}
