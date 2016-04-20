3ae7407ca08bc1cd96fc67c278529476b453d3b6
liuzhuocpp <liuzhuocpp@qq.com>
2016-04-20 16:45:13 +0800

    admin 签到完善了

--- DuoMeiTi/WebContent/jsp/student/checkin.jsp
+++ DuoMeiTi/WebContent/jsp/student/checkin.jsp
@@ -1,7 +1,7 @@
 <%@ include file="/jsp/base/taglib.jsp" %>
 <layout:override name="main_content">
 
-<script type='text/javascript' src="/js/student/checkin.js"></script>
+
 <div class="mycontent">
 	<h>学生签到</h>
 	<button type="button" class="btn btn-default"  style="margin:2px;" id="addbutton" onclick="checkin()">签&nbsp;&nbsp;到</button>
@@ -12,30 +12,49 @@
 		<div class="student_table" id="studnetcheckintable">
 			<%@ include file="/jsp/student/studentcheckintable.jsp" %>
 		</div>
-		<div>
-			<%@ include file="/jsp/base/widgets/pagination.jsp" %>
-		</div>
+<!-- 		<div> -->
+<%-- 			<%@ include file="/jsp/base/widgets/pagination.jsp" %> --%>
+<!-- 		</div> -->
 	</div>
-	<script>
+	
+	
+<!-- 	<script type='text/javascript' src="/js/student/checkin.js"></script> -->
+<script>
+
+function checkin(){
+	$.ajax({
+		url: "/studentcheckin/checkinbyusername",
+		success: result,
+		error: result
+	});
+}
+
+function result(data){
+	alert(data.result);
+	location.reload();
+}
+
+
+
 	// 你可以定义pageAddtionalData变量，这个变量应该是json变量，这个变量可以直接通过ajax 在选择页码的时候传到后台
 	// 你应该重写下面这个函数，使其在回调的时候可以做你自己做的事情
 	
-		function sendRequestPage(currentPageNum) {
-		var data = {"currentPageNum": currentPageNum , "isAjaxTransmission":true, };
-		if(typeof(pageAddtionalData)!="undefined")
-			data = $.extend({}, data, pageAddtionalData);
-	    $.ajax({
-	        url: paginationURL,
-	        type: 'post',
-	        dataType: 'json',
-	        data: data,
-	        success: _requestPageCallback,
-	        error: requesterror
-	      });	
-	}
-		function requestPageCallback(data){
-			document.getElementById("studnetcheckintable").innerHTML=data.newtablestring;
-		}
+// 		function sendRequestPage(currentPageNum) {
+// 		var data = {"currentPageNum": currentPageNum , "isAjaxTransmission":true, };
+// 		if(typeof(pageAddtionalData)!="undefined")
+// 			data = $.extend({}, data, pageAddtionalData);
+// 	    $.ajax({
+// 	        url: paginationURL,
+// 	        type: 'post',
+// 	        dataType: 'json',
+// 	        data: data,
+// 	        success: _requestPageCallback,
+// 	        error: requesterror
+// 	      });	
+// 	}
+// 		function requestPageCallback(data){
+// 			document.getElementById("studnetcheckintable").innerHTML=data.newtablestring;
+// 		}
 </script>
 
 
---
+++ DuoMeiTi/src/student/CheckinManageAction.java
@@ -0,0 +1,204 @@
+package student;
+
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import checkin.TimeUtil;
+import model.CheckInRecord;
+import util.Util;
+
+
+
+
+public class CheckinManageAction extends ActionSupport{
+	
+	public String execute() throws Exception
+	{
+		return ActionSupport.SUCCESS;
+	}
+	
+	
+	java.util.Date startTime;
+	java.util.Date endTime;
+	String stuName;
+	
+	String checkinRecordTable;
+	List checkinRecordList;
+	public String search() throws Exception
+	{
+		
+		System.out.println("SB-------");
+		Session session = model.Util.sessionFactory.openSession();
+		Criteria c = session.createCriteria(CheckInRecord.class)		
+							.addOrder(Order.desc("id"));
+				
+		
+		if(!stuName.equals(""))
+		{
+			c.createAlias("student", "student")
+			.createAlias("student.user", "user")
+			.add(Restrictions.eq("user.fullName", stuName));
+		}
+			
+		if(startTime != null)
+		{
+			c.add(Restrictions.ge("recordtime", startTime));
+		}
+		
+		if(endTime != null)
+		{
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(endTime);
+			cal.add(Calendar.DATE, 1);			
+			c.add(Restrictions.le("recordtime", cal.getTime()));
+		}
+		
+		checkinRecordList = c.list();
+
+		
+		checkinRecordTable = Util.getJspOutput("/jsp/admin/widgets/checkinRecordTable.jsp");
+		session.close();
+		return ActionSupport.SUCCESS;
+	}
+	
+	
+	String checkinRecordExcelPath;
+	public String exportExcel() throws Exception{
+
+		HSSFWorkbook wb = new HSSFWorkbook();
+		HSSFSheet sheet = wb.createSheet("签到记录");
+		HSSFRow row = sheet.createRow((int) 0);  
+        HSSFCellStyle style = wb.createCellStyle();  
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER); // 创建一个居中格式  
+        
+        HSSFCell cell = row.createCell(0, Cell.CELL_TYPE_STRING);  
+        cell.setCellValue("姓名");  
+        cell.setCellStyle(style);  
+        cell = row.createCell(1, Cell.CELL_TYPE_STRING);  
+        cell.setCellValue("学号");  
+        cell.setCellStyle(style);  
+        cell = row.createCell(2, Cell.CELL_TYPE_STRING);  
+        cell.setCellValue("签到时间");  
+        cell.setCellStyle(style);
+
+        
+        this.search();        
+
+        
+		for(int i = 0; i < checkinRecordList.size(); ++i) {
+//			System.out.println(L.get(i));
+			row = sheet.createRow((int) i + 1);  
+			CheckInRecord cr = (CheckInRecord) checkinRecordList.get(i);
+            row.createCell(0, Cell.CELL_TYPE_STRING).setCellValue(cr.getStudent().getUser().getFullName());  
+            row.createCell(1, Cell.CELL_TYPE_STRING).setCellValue(cr.getStudent().getStudentId());            
+            row.createCell(2, Cell.CELL_TYPE_STRING).setCellValue(util.Util.formatTimestamp(cr.recordtime));
+		}
+				
+		checkinRecordExcelPath = util.Util.CheckInExcelExportPath + "签到记录.xls";
+		
+		try  
+        {
+			
+            FileOutputStream fout = new FileOutputStream(util.Util.RootPath + checkinRecordExcelPath);  
+            wb.write(fout);  
+            fout.close();
+            wb.close();
+        }
+		catch (Exception e)  
+        {
+			e.printStackTrace();
+        }
+
+		
+		
+		return SUCCESS;
+	}
+
+	
+	
+	
+	
+
+	
+	public java.util.Date getStartTime() {
+		return startTime;
+	}
+
+
+	public void setStartTime(java.util.Date startTime) {
+		this.startTime = startTime;
+	}
+
+
+	public java.util.Date getEndTime() {
+		return endTime;
+	}
+
+
+	public void setEndTime(java.util.Date endTime) {
+		this.endTime = endTime;
+	}
+
+
+	public String getStuName() {
+		return stuName;
+	}
+
+
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+
+
+	public String getCheckinRecordTable() {
+		return checkinRecordTable;
+	}
+
+
+	public void setCheckinRecordTable(String checkinRecordTable) {
+		this.checkinRecordTable = checkinRecordTable;
+	}
+
+
+	public List getCheckinRecordList() {
+		return checkinRecordList;
+	}
+
+
+	public void setCheckinRecordList(List checkinRecordList) {
+		this.checkinRecordList = checkinRecordList;
+	}
+	public String getCheckinRecordExcelPath() {
+		return checkinRecordExcelPath;
+	}
+	public void setCheckinRecordExcelPath(String checkinRecordExcelPath) {
+		this.checkinRecordExcelPath = checkinRecordExcelPath;
+	}
+	
+}
