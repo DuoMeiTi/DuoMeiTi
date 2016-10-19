06e01c4fe819b0ca132ee968b9e1e08d38f9eb84
liuzhuocpp <liuzhuocpp@qq.com>
2016-10-19 10:37:44 +0800

    make it better

--- DuoMeiTi/src/homepage/ClassroomDetailAction.java
+++ DuoMeiTi/src/homepage/ClassroomDetailAction.java
@@ -28,131 +28,34 @@ import util.FileUploadBaseAction;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
-//import db.MyHibernateSessionFactory;
-
-public class ClassroomDetailAction extends FileUploadBaseAction{
-	public String build_name;
-	public String remark;
-	public int picID;
-	public static int classroomId;
+public class ClassroomDetailAction extends FileUploadBaseAction {
 	
-	public TeachBuilding building;
+	public int classroomId;
 	public Classroom classroom;
-	
 	public List<CheckRecord> checkrecords;
-	
 	public List<RepairRecord> repairrecords;
-	public List<RoomPicture>picture_list;
-	public List classroom_device_list;
-	
-	public List<Repertory> rtClass;
-	public String class_schedule_path ;
-	
-	
-
+	public List<RoomPicture> picture_list;
+	public List<Repertory> classroom_device_list;
+	public String class_schedule_path;
 
 	public String execute() {
 		Session session = model.Util.sessionFactory.openSession();
-		
-		//query current select classroom
+
+		// query current select classroom
 		classroom = common.ClassroomInformationAction.obtainClassroom(session, classroomId);
-		
-		
-		//课程表
+
+		// 课程表
 		class_schedule_path = classroom.class_schedule_path;
-		
-		classroom_device_list = 
-				common.ClassroomInformationAction.obtainClassroomDeviceListCriteria(session, classroomId).list();
-		
+
+		classroom_device_list = common.ClassroomInformationAction
+				.obtainClassroomDeviceListCriteria(session, classroomId).list();
+
 		checkrecords = common.ClassroomInformationAction.obtainCheckRecordListCriteria(session, classroomId).list();
 		repairrecords = common.ClassroomInformationAction.obtainRepairRecordListCriteria(session, classroom).list();
-		
-
-		
-		picture_list = session.createCriteria(RoomPicture.class).add(Restrictions.eq("class_id",classroomId)).list();
-
-
+		picture_list = session.createCriteria(RoomPicture.class).add(Restrictions.eq("class_id", classroomId)).list();
 		session.close();
-		
+
 		return ActionSupport.SUCCESS;
-	}
-	
-	
-//	public void ClassroomPicture(){
-//		
-//		Session session = model.Util.sessionFactory.openSession();
-//		session.close();
-//		
-//		System.out.println(picture_list);
-//	}
-//	
-//	public String PictureUpload() {
-//		System.out.println("PictureUpload!");
-//		System.out.println("calssid" + classroomId);
-//		System.out.println(remark);
-//		
-//		Session session = model.Util.sessionFactory.openSession();
-//		RoomPicture nPicture = new RoomPicture();	
-//		
-//		nPicture.setClass_id(classroomId);
-//		nPicture.setRemark(remark);
-//		
-//		//获取当前时间，命名照片，防止照片重复
-//		java.util.Date date = new java.util.Date();
-//		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddHHmmss");
-//		String curdate = simpleDateFormat.format(date);
-//		String fileName = curdate+fileFileName;
-//
-//		if (file != null)//file没接收到的原因可能是jsp页面里面的input file的属性名不是file 
-//        {
-//			util.Util.saveFile(file, fileName, util.Util.RootPath + util.Util.ClassroomInfoFilePath);
-//			String inserted_file_path = util.Util.ClassroomInfoFilePath +fileName;
-//			nPicture.setPath(inserted_file_path);
-//        }
-//		
-//		session.beginTransaction();
-//		session.save(nPicture);
-//		Transaction t = session.getTransaction();
-//		t.commit();
-//		
-//		return ActionSupport.SUCCESS;
-//	}
-//	
-//	
-//	
-//	public String PictureDelete() {
-//		System.out.println("PictureDelete:");
-//		System.out.println(picID);
-//		
-//		//从数据库中删除
-//		Session session = model.Util.sessionFactory.openSession();		
-//		Criteria q = session.createCriteria(RoomPicture.class).add(Restrictions.eq("id",picID)); //hibernate session创建查询
-//		picture_list=q.list();
-//		Collections.reverse(picture_list);
-//		RoomPicture nPicture = new RoomPicture();
-//		nPicture = picture_list.get(0);
-//		//System.out.println(nPicture);
-//		session.beginTransaction();
-//		session.delete(nPicture);
-//		Transaction t = session.getTransaction();
-//		t.commit();
-//		session.close();
-//		//删除本地文件
-//		String filePath = util.Util.RootPath + nPicture.getPath();
-//		System.out.println(filePath);
-//		util.Util.deleteFile(filePath);
-//		
-//		return ActionSupport.SUCCESS;
-//	}
-//	
-
-	
-	public String getBuild_name() {
-		return build_name;
-	}
-
-	public void setBuild_name(String build_name) {
-		this.build_name = build_name;
 	}
 
 	public int getClassroomId() {
@@ -162,12 +65,53 @@ public class ClassroomDetailAction extends FileUploadBaseAction{
 	public void setClassroomId(int classroomId) {
 		this.classroomId = classroomId;
 	}
+
+	public Classroom getClassroom() {
+		return classroom;
+	}
+
+	public void setClassroom(Classroom classroom) {
+		this.classroom = classroom;
+	}
+
+	public List<CheckRecord> getCheckrecords() {
+		return checkrecords;
+	}
+
+	public void setCheckrecords(List<CheckRecord> checkrecords) {
+		this.checkrecords = checkrecords;
+	}
+
+	public List<RepairRecord> getRepairrecords() {
+		return repairrecords;
+	}
+
+	public void setRepairrecords(List<RepairRecord> repairrecords) {
+		this.repairrecords = repairrecords;
+	}
+
+	public List<RoomPicture> getPicture_list() {
+		return picture_list;
+	}
+
+	public void setPicture_list(List<RoomPicture> picture_list) {
+		this.picture_list = picture_list;
+	}
+
+	public List getClassroom_device_list() {
+		return classroom_device_list;
+	}
+
+	public void setClassroom_device_list(List classroom_device_list) {
+		this.classroom_device_list = classroom_device_list;
+	}
+
 	public String getClass_schedule_path() {
 		return class_schedule_path;
 	}
 
-
 	public void setClass_schedule_path(String class_schedule_path) {
 		this.class_schedule_path = class_schedule_path;
 	}
+
 }
